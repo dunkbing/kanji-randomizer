7b99573daa9787fe2e119cf923156c5f9382374f
dunkbing <dangbinh4869@gmail.com>
2020-05-23 15:18:19 +0700

    modified styles

---
+++ script.js
@@ -0,0 +1,63 @@
+const grades = [1, 2, 3, 4, 5, 6, 8]
+const loading = document.querySelector("#loading")
+const kanjiInfo = document.querySelector("#kanjiInfo")
+const kanjiEle = document.querySelector("#kanji")
+const gradeEle = document.querySelector("#grade")
+const jlptEle = document.querySelector("#jlpt")
+const kunEle = document.querySelector("#kun-readings")
+const nameReadingEle = document.querySelector("#name-readings")
+const onEle = document.querySelector("#on-readings")
+const meaningsEle = document.querySelector("#meanings")
+
+async function fetchKanji(grade){
+    displayLoading(true)
+    const kanjiByGrade = await fetch(`https://kanjiapi.dev/v1/kanji/grade-${grade}`)
+    const kanjiArr = await kanjiByGrade.json()
+    const res = await fetch(`https://kanjiapi.dev/v1/kanji/${kanjiArr[randomRange(0, kanjiArr.length-1)]}`)
+    return await res.json()
+}
+
+window.onload = function(){
+    this.displayLoading(false)
+    const kan = {
+        grade: 3, 
+        jlpt: 3, 
+        kanji: "漢", 
+        kun_readings: [], 
+        name_readings: ["はん"], 
+        on_readings: ["カン"], 
+        meanings: ["Sino-", "China"]
+    }
+    this.loadKanji(kan)
+}
+document.querySelector(".kanji").onclick = async function(){
+    try {
+        const kanji = await fetchKanji(grades[randomRange(0, grades.length-1)])
+        console.log(kanji)
+        loadKanji(kanji)
+        displayLoading(false)
+    } catch (error) {
+        alert("error, pls try again")
+        //console.log(error)
+    }
+    
+}
+
+function randomRange(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function displayLoading(bool){
+    loading.style.display = bool ? "block" : "none"
+    kanjiInfo.style.display = bool ? "none" : "block"
+}
+
+function loadKanji({grade, jlpt, kanji, kun_readings, name_readings, on_readings, meanings}){
+    kanjiEle.innerText = kanji
+    gradeEle.innerText = `grade:${grade}`
+    jlptEle.innerText = `jlpt:${jlpt}`
+    kunEle.innerText = `${kun_readings.join(" | ")}`
+    nameReadingEle.innerText = `${name_readings.join(" | ")}`
+    onEle.innerText = `${on_readings.join(" | ")}`
+    meaningsEle.innerText = `${meanings.join(" | ")}`
+}
